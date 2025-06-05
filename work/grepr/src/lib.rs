6d49eca3faa04de4756eb3ae9dc998e73f04a4f2
sAbIz <bellett@sabiz.jp>
2025-06-05 21:18:38 +0900

    add find files

--- work/grepr/src/lib.rs
+++ work/grepr/src/lib.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
 use regex::{Regex, RegexBuilder};
-use std::error::Error;
+use walkdir::WalkDir;
+use std::{error::Error, fmt::format, fs::File, io::{BufRead, BufReader}, path::PathBuf};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -14,7 +15,15 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    println!("Pattern: {}", config.pattern);
+
+    let entries = find_files(&config.files, config.recursive);
+    for entry in entries {
+        match entry {
+            Err(e)=> eprintln!("Error: {}", e),
+            Ok(filename) => println!("file: {}", filename),
+        }
+    }
     Ok(())
 }
 
@@ -78,4 +87,124 @@ pub fn get_args() -> MyResult<Config> {
         count: matches.is_present("count"),
         invert_match: matches.is_present("invert-match"),
     })
+}
+
+fn find_files(path: &[String], recursive: bool) -> Vec<MyResult<String>> {
+    path.iter().map(|p| {
+        if p == "-" {
+            return vec![Ok("-".to_string())];
+        }
+        let path = PathBuf::from(p);
+        if !path.exists() {
+            return vec![Err(format!("{} does not exist", p).into())];
+        }
+        if path.is_file() {
+            return vec![Ok(p.clone())];
+        }
+        if path.is_dir() {
+            if !recursive {
+                return vec![Err(format!("{} is a directory", p).into())];
+            }
+            let dirs:Vec<_> = WalkDir::new(path).min_depth(1).into_iter().filter_map(Result::ok).map(|p| {p.path().to_string_lossy().to_string()}).collect();
+            return find_files(&dirs, true);
+        }
+        vec![Ok(p.clone())]
+    }).flatten().collect()
+}
+
+fn find_lines<T: BufRead> (mut file: T, pattern: &Regex, invert_match: bool) -> MyResult<Vec<String>> {
+    unimplemented!();
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{find_files, find_lines};
+    use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_find_lines() {
+        let text = b"Lorem\nIpsum\r\nDOLOR";
+
+        // The pattern _or_ should match the one line, "Lorem"
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+
+        // When inverted, the function should match the other two lines
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // This regex will be case-insensitive
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+
+        // The two lines "Lorem" and "DOLOR" should match
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+
+        // When inverted, the one remaining line should match
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+    }
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let files =
+            find_files(&["./tests/inputs/fox.txt".to_string()], false);
+        assert_eq!(files.len(), 1);
+        assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
+
+        // The function should reject a directory without the recursive option
+        let files = find_files(&["./tests/inputs".to_string()], false);
+        assert_eq!(files.len(), 1);
+        if let Err(e) = &files[0] {
+            assert_eq!(e.to_string(), "./tests/inputs is a directory");
+        }
+
+        // Verify the function recurses to find four files in the directory
+        let res = find_files(&["./tests/inputs".to_string()], true);
+        let mut files: Vec<String> = res
+            .iter()
+            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .collect();
+        files.sort();
+        assert_eq!(files.len(), 4);
+        assert_eq!(
+            files,
+            vec![
+                "./tests/inputs/bustle.txt",
+                "./tests/inputs/empty.txt",
+                "./tests/inputs/fox.txt",
+                "./tests/inputs/nobody.txt",
+            ]
+        );
+
+        // Generate a random string to represent a nonexistent file
+        let bad: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        // Verify that the function returns the bad file as an error
+        let files = find_files(&[bad], false);
+        assert_eq!(files.len(), 1);
+        assert!(files[0].is_err());
+    }
 }
